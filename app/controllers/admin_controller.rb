1834c8f543542a5e91b5b29c2ca8e894fb4e03e2
Derek Pressnall <derekpressnall@yahoo.com>
2016-02-08 14:24:44 -0600

    No sure what to do. Scratching my head waiting for something to pop out. Kind of afraid to tinker with the code cause I don't want to severely fuck it up. Not sure if thats the point. Where to start? Got the back button working. Big strides over here LOL.
    Bleep bloop bleep bloop.

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -17,6 +17,22 @@ MyApp.get "/assignments" do
   erb :"/admin/assignment"
 end
 
-MyApp.get "/assignments/1" do
-  erb :"/admin/prs_assignment"
+MyApp.get "/assignment/1" do
+  erb :"/admin/view_assignment"
+end
+
+MyApp.get "/assignment/2" do
+  erb :"/admin/str_seg_assignment"
+end
+
+MyApp.get "/assignment/3" do
+  erb :"/admin/prep_assignment"
+end
+
+MyApp.get "/assignment/4" do
+  erb :"/admin/sinatra_assignment"
+end
+
+MyApp.get "/assignment/5" do
+  erb :"/admin/data_assignment"
 end
